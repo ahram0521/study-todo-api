40092c67cd2714198edc28138f1feaf8a3c04f1b
ahram0521 <dkfka5531@gmail.com>
2025-05-06 20:02:52 +0900

    build: [#1] 공통 파일 생성 & docker 연결 및 설정 변경

--- src/main/java/dev/study/todoapi/TodoapiApplication.java
+++ src/main/java/dev/study/todoapi/TodoapiApplication.java
@@ -2,7 +2,10 @@ package dev.study.todoapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class TodoapiApplication {
 
